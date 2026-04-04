1eb3567f5dffa0eeb24b24dd07ab5b974bb4b44e
Bošnjak Borislav <50413711+BorisOrbis@users.noreply.github.com>
2026-04-04 15:12:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'boris-v1';
+const CACHE = 'track-v2';
 const ASSETS = [
   '/track/',
   '/track/index.html',
@@ -10,22 +10,23 @@ const ASSETS = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => Promise.allSettled(ASSETS.map(a => c.add(a))))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
-      caches.match('/track/index.html')
-    ))
+    caches.match(e.request)
+      .then(r => r || fetch(e.request).catch(() => caches.match('/track/index.html')))
   );
 });
